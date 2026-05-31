d360a73e0a626b3b1e2364598cc68cdd3b5cf48f
joydesouza <joy.dsouza@smallcase.com>
2026-05-31 14:02:28 +0530

    feat: add pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
-  site: 'https://clientdomain.com',
+  site: 'https://www.indvaltech.com',
   output: 'static',
   vite: {
     plugins: [tailwindcss()],
